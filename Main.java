f4e9b354392d433b2ef5218786aefaef3139133c
jdhopia <43420804+jdhopia@users.noreply.github.com>
2024-08-04 20:34:23 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,9 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         SimulationController controller = new SimulationController();
-        controller.initializeCharacters(17, 8);
-        System.out.println("We have 17 survivors trying to make it to safety.");
-        System.out.println("But there are 8 zombies waiting for them.");
+        controller.initializeCharacters();
         controller.printInitialStatus();
         controller.runSimulation();
         controller.reportResults();
--- SimulationController.java
+++ SimulationController.java
@@ -1,35 +1,47 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Random;
 
 // class to control the simulation of the zombie war
 public class SimulationController {
     private List<Character> survivors;
     private List<Character> zombies;
+    private Random random;
 
     // initializes the list of survivors and zombies
     public SimulationController() {
         survivors = new ArrayList<>();
         zombies = new ArrayList<>();
+        random = new Random();
     }
 
     // method to initialize characters with random types
-    public void initializeCharacters(int numSurvivors, int numZombies) {
-        Random rand = new Random();
-        for (int i = 0; i < numSurvivors; i++) {
-            int survivorType = rand.nextInt(3);
-            switch (survivorType) {
-                case 0 -> survivors.add(new Soldier());
-                case 1 -> survivors.add(new Civilian());
-                case 2 -> survivors.add(new Scientist());
-            }
+    public void initializeCharacters() {
+        // Randomize the number of each type of survivor and zombie
+        int numScientists = random.nextInt(5);
+        int numCivilians = random.nextInt(5);
+        int numSoldiers = random.nextInt(5);
+        int numCommonInfected = random.nextInt(5);
+        int numTanks = random.nextInt(5);
+
+        // Add survivors
+        for (int i = 0; i < numScientists; i++) {
+            survivors.add(new Scientist());
         }
-        for (int i = 0; i < numZombies; i++) {
-            int zombieType = rand.nextInt(2);
-            switch (zombieType) {
-                case 0 -> zombies.add(new CommonInfected());
-                case 1 -> zombies.add(new Tank());
-            }
+        for (int i = 0; i < numCivilians; i++) {
+            survivors.add(new Civilian());
+        }
+        for (int i = 0; i < numSoldiers; i++) {
+            survivors.add(new Soldier());
+        }
+
+        // Add zombies
+        for (int i = 0; i < numCommonInfected; i++) {
+            zombies.add(new CommonInfected());
+        }
+        for (int i = 0; i < numTanks; i++) {
+            zombies.add(new Tank());
         }
     }
 
@@ -38,43 +50,48 @@ public class SimulationController {
         int survivorCounter = 0;
         int zombieCounter = 0;
 
+        // Survivors attack zombies
         for (Character survivor : survivors) {
+            zombieCounter = 0;
             for (Character zombie : zombies) {
                 if (survivor.isAlive() && zombie.isAlive()) {
                     survivor.attack(zombie);
                     if (!zombie.isAlive()) {
                         System.out.println(survivor.getClass().getSimpleName() + " " + survivorCounter + " killed " + zombie.getClass().getSimpleName() + " " + zombieCounter);
                     }
-                    zombieCounter++;
                 }
+                zombieCounter++;
             }
             survivorCounter++;
         }
+
         survivorCounter = 0;
         zombieCounter = 0;
 
+        // Zombies attack survivors
         for (Character zombie : zombies) {
+            survivorCounter = 0;
             for (Character survivor : survivors) {
                 if (zombie.isAlive() && survivor.isAlive()) {
                     zombie.attack(survivor);
                     if (!survivor.isAlive()) {
                         System.out.println(zombie.getClass().getSimpleName() + " " + zombieCounter + " killed " + survivor.getClass().getSimpleName() + " " + survivorCounter);
                     }
-                    survivorCounter++;
                 }
+                survivorCounter++;
             }
             zombieCounter++;
         }
     }
 
+
     // method to report the results of the war
     public void reportResults() {
         long survivorsAlive = survivors.stream().filter(Character::isAlive).count();
-        if (survivorsAlive > 0) {
-            System.out.println("It seems " + survivorsAlive + " have made it to safety.");
-        }
-        else{
+        if (survivorsAlive == 0) {
             System.out.println("None of the survivors made it.");
+        } else {
+            System.out.println("It seems " + survivorsAlive + " have made it to safety.");
         }
     }
 
