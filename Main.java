0561421ed4c8059940381101741e1ee68e684231
jdhopia <43420804+jdhopia@users.noreply.github.com>
2024-08-04 20:25:27 -0500

    Add files via upload

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
         controller.initializeCharacters(17, 8);
         System.out.println("We have 17 survivors trying to make it to safety.");
         System.out.println("But there are 8 zombies waiting for them.");
+        controller.printInitialStatus();
         controller.runSimulation();
         controller.reportResults();
     }
